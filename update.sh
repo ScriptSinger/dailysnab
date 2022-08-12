71234d9df90cc88eb6053bfc07971cf13016b29a
Vladislav <perumn@mail.ru>
2022-08-12 10:51:28 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ~/questrequest.ru/public_html/; 
 ls -l
-# git add .
-# git commit -m 'Изменения Димы и с сервера'
-# git push
+git add .
+git commit -m 'Изменения Димы и с сервера'
+git push
 
-# git pull
+git pull
