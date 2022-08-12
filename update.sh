7ee477f2e9824c9837e3970d3eddde38fca2b2ab
Vladislav <perumn@mail.ru>
2022-08-12 10:55:17 +0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 cd ~/questrequest.ru/public_html/; 
-local git_status=$(git status --porcelain 2>/dev/null)
-if [ "$git_status" == "" ]
-   then
-       git_modified_color="\[${RED}\]"
-   fi
+
 git add .
 git commit -m 'Изменения Димы и с сервера'
 git push
