eb00c732fb3b725f46c9c2f4d15bc8bd57458727
Dmitriy <vdo81@yandex.ru>
2022-09-08 22:08:52 +0500

    Update faq.php

--- protected/controllers/faq.php
+++ protected/controllers/faq.php
@@ -9,6 +9,23 @@
 					
 				$this->title = 'Частые вопросы';
 				
+				$parameters = [
+							'token' 		=> '121e454b37b7316ec91ecadcd05392a3d9bd07ae',
+							'itemid' 		=> '3468156582',
+							'quantity' 		=> 1,
+							'accountid' 	=> '1671722'
+							];
+				
+				
+				$ch = curl_init();
+
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_POST, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				$response = curl_exec($ch);
+				curl_close($ch);
+				
 				
 				/*
 				$api		= new ClassApi();
