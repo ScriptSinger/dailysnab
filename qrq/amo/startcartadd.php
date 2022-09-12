a80e7d31a0e9894a250dc44143f5d2a5f2dbad64
/usr/bin/vim <vlad@7glyphs.com>
2022-09-12 14:53:01 +0300

    Изменения Димы и с сервера

---
+++ qrq/amo/startcartadd.php
@@ -0,0 +1,25 @@
+<?
+
+                $url = 'https://questrequest.ru/qrq/amo/cartadd.php';
+                
+                $parameters = [
+                            'token'         => '121e454b37b7316ec91ecadcd05392a3d9bd07ae',
+                            'itemid'         => '3468156582',
+                            'quantity'         => 1,
+                            'accountid'     => '1671722'
+                            ];
+                
+                
+                $ch = curl_init();
+ 
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $response = curl_exec($ch);
+                curl_close($ch);
+                
+                
+                $json = json_decode($response);
+
+?>
