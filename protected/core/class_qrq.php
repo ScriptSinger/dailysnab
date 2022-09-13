1a1407d6ac1d9ac7d73f54444cb7b52e0c573d9e
Dmitriy <vdo81@yandex.ru>
2022-09-13 23:44:38 +0500

    Update class_qrq.php

--- protected/core/class_qrq.php
+++ protected/core/class_qrq.php
@@ -774,17 +774,17 @@ class ClassQrq extends HtmlServive
 
 				$json = self::getJsonCurl(array('url'=>$url,'parameters'=>$parameters));
 				//vecho($parameters);
-				vecho($json);
+				//vecho($json);
 				if($json){
 					
 					// пишем лог
 					$ins_id = reqInsertAmoLogJson(array('url'=>'cartadd','parameters'=>$parameters,'json'=>$json,'token'=>AMO_TOKEN));
 					
 					//vecho($parameters);
-					//vecho($json);
+					
 					
 					$Response	= isset($json->Response)? $json->Response : '';
-					
+					vecho($Response);
 					$errors = isset($Response->errors)? $Response->errors : '';
 					$errors_message 	= isset($errors[0]->message)?	 	$errors[0]->message 		: '';
 
