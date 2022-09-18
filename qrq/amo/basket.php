6af27467678c666367047c5b345299fff695bb68
/usr/bin/vim <vlad@7glyphs.com>
2022-09-18 19:29:01 +0300

    Изменения Димы и с сервера

--- qrq/amo/basket.php
+++ qrq/amo/basket.php
@@ -677,7 +677,9 @@ else
         <script>
         function funButNew()
         {
+            alert('111');
             var pRestParam = '{"Response":{"errors":["code":"222","message":"AccountId='.$pAccountId.' ушел только в корзину","details":null],"warnings":null}}';
+            alert('222 = '+pRestParam);
             AmoBasket(request.responseText,<? echo $pAmount; ?>,<? echo $pBuy_sell_id; ?>,<? echo '"'.$pWhere.'"'; ?> , <? echo $pAccountId; ?> , pRestParam );
         }
         </script>
