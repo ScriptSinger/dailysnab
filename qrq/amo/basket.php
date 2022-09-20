8a68439f942d58db1fee149633240cb640c8e93a
/usr/bin/vim <vlad@7glyphs.com>
2022-09-20 15:04:00 +0300

    Изменения Димы и с сервера

--- qrq/amo/basket.php
+++ qrq/amo/basket.php
@@ -173,13 +173,13 @@ if ( (strlen(trim($restaccount))>0) && (strlen(trim($restvendor))>0))
 
 
 
-        /*
+        
             echo '<pre>';
             print_r($Resp);
             echo '</pre>';
             echo "<br>";
-            echo "<br>";
-        */
+           echo "<br>";
+        
 
         $error = $Resp['Response']['errors'];
         $warnings = $Resp['Response']['warnings'];
@@ -592,7 +592,7 @@ if ( (strlen(trim($restaccount))>0) && (strlen(trim($restvendor))>0))
 
                         // надо вставить эту переменную
                         // pRestParam
-                        AmoBasket(request.responseText,<? echo $pAmount; ?>,<? echo $pBuy_sell_id; ?>,<? echo '"'.$pWhere.'"'; ?> , <? echo $pAccountId; ?> , pRestParam );
+                     
 
 
 
