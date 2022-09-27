3fb3eef067b7b1575b4ec33bcc8d9b79db00b9f1
/usr/bin/vim <vlad@7glyphs.com>
2022-09-27 21:17:01 +0300

    Изменения Димы и с сервера

--- protected/views/sell22.php
+++ protected/views/sell22.php
@@ -65,6 +65,10 @@
 															if(data.code){
 																$("#div_tr_sell").html(data.code);
 																$("#last_etp_id").val(data.id);
+																if(data.noload){
+																	alert("qw");
+																	//$("#loading").hide();
+																}
 															}
 															timeOutId3 = setTimeout(ajaxFn3, 3000);
 												}
