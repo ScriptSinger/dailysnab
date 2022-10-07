c3752dd6e49ae684b720d8324a6aed5225570807
/usr/bin/vim <vlad@7glyphs.com>
2022-10-07 13:52:02 +0300

    Изменения Димы и с сервера

--- qrq/amo/basketnew.php
+++ qrq/amo/basketnew.php
@@ -132,7 +132,7 @@ function fnCountMass($pJson,$pBeginId,$pEndtId)
      {
 
         echo '<script>';
-        echo ' var insSelect = document.getElementById("select'.$pValue.'"); ';
+        echo ' var insSelect = document.getElementById("select'.$pBeginId.'"); ';
         echo ' var insOption = document.createElement("option"); ';
         echo " insOption.innerHTML = '".$pTitleText.'-'.$pValue."'; "; 
         echo " insOption.value = '".$pBeginId."'; ";
