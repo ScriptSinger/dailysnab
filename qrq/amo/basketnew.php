a826a9c6e475b5168db064d074ef07d89845f7d7
/usr/bin/vim <vlad@7glyphs.com>
2022-10-07 10:47:01 +0300

    Изменения Димы и с сервера

--- qrq/amo/basketnew.php
+++ qrq/amo/basketnew.php
@@ -112,20 +112,14 @@ $Resp = json_decode($response,true);
   
   $count = count($serg);
   echo "count = "+$count;
-     echo "<br>";
+  echo "<br>";
+  
   foreach ($serg as $value) 
   {
      echo $value['title'];
      echo "<br>";
   }  
 
-/*  
-foreach ($serg as $key => $value) {
-    // $arr[3] будет перезаписываться значениями $arr при каждой итерации цикла
-    echo "{$key} => {$value} ";
-    print_r($arr);
-}
-*/
 
 
   
