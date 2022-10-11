49e09e11d69617feb2272e6c4c55561835c7349e
/usr/bin/vim <vlad@7glyphs.com>
2022-10-11 21:58:01 +0300

    Изменения Димы и с сервера

--- protected/core/ajax.php
+++ protected/core/ajax.php
@@ -2615,14 +2615,14 @@ elseif($_GET['route'] == 'search_brend_etp'){
             $at = array(386,389,388);// допустимые категории 3 уровнея
             if(in_array($rc['parent_id'], $at)){//
 
-                if(!COMPANY_ID){
+                /*if(!COMPANY_ID){
                     if(!AMO_TOKEN){
                         $qrq		= new ClassQrq();
                         $token 	= $qrq->AuthorizationAmo();
                         $_SESSION['AMO_TOKEN'] 	= $token;
                     }
 
-                }
+                }*/
 
                 $arr = $qrq->AmoHtmlSearchbrend(array('flag'			=> 2,
 													'token'			=> $_SESSION['AMO_TOKEN'],
