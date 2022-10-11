ed955a6b33242dcae9adc1a1829c501a4dd410c3
/usr/bin/vim <vlad@7glyphs.com>
2022-10-11 21:56:01 +0300

    Изменения Димы и с сервера

--- protected/core/Requests.php
+++ protected/core/Requests.php
@@ -7411,8 +7411,7 @@
 			$one = true;
 		}
 
-		$sql = "	SELECT ci.id, ci.token, ci.searchid, ci.brand, ci.searchtext, ci.accountid, 
-						ci.categories_id, ci.company_id_out, ci.cookie_session
+		$sql = "	SELECT ci.id, ci.token, ci.searchid, ci.categories_id, ci.company_id_out, ci.cookie_session
 				FROM cron_amo_buy_sell_search_infopart ci
 				WHERE 1=1 ".$sql." ";
 
