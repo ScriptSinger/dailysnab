a40a438d1a07e0c299b8855cc230ac424da36623
/usr/bin/vim <vlad@7glyphs.com>
2022-10-14 11:15:00 +0300

    Изменения Димы и с сервера

--- protected/controllers/chat.php
+++ protected/controllers/chat.php
@@ -1,89 +1,80 @@
 <?php // Подписки
-	class Controllers_Chat extends core_BaseController
-	{
-		function __construct()
-		{
-			parent::__construct();
-			
-			if(LOGIN_ID){				
-				
-							$views = getArgs(1);							
-							$views = ($views)? $views : 'messages';
-							$value 	= getGets('value');
-							$mid = $value = '';	
+class Controllers_Chat extends core_BaseController
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-							$mid 	= getArgs(2);
-							//$mid = (!empty($mid))? $mid : '';
-							//$_mid	= getGets('messages_id');
-							
-							
+        if (LOGIN_ID) {
 
-							// страница - Все сообщения
-							if($views=='messages' ){
-							
-									// Поиск
+            $views = getArgs(1);
+            $views = ($views) ? $views : 'messages';
+            $value = getGets('value');
+            $mid = $value = '';
 
-								 	$row  = $this->rowChatMessages(array( 	'views' 			=> $views,
+            $mid = getArgs(2);
+            //$mid = (!empty($mid))? $mid : '';
+            //$_mid    = getGets('messages_id');
 
-																			'value' 			=> $value)); 									
+            // страница - Все сообщения
+            if ($views == 'messages') {
 
-										
-									$rown = reqChatMessages(array( 	'views' 	=> $views,
-																	'folder_id'	=> $mid																				
-																));	
-															
-																			
-									$this->chat = array(	'views'	=> $views,
-															'row' 	=> $row,
-															'value' => $value,
-															'mid' 	=> $mid,
-															'rown' 	=> $rown
-																				);
-									$this->title = 'Все сообщения';
-							}					
-							// страница - Отктыре темы
-							elseif($views=='open-chats'){
-									$row = $this->rowChatMessages(array( 	'views' => $views));
-									$this->chat = array(	'views'	=> $views,
-															'row' 	=> $row,
-															'value' => $value
-																				);
-									$this->title = 'Отктыре темы';
-							}
-							// страница - Архивные
-							elseif($views=='arhive-chats'){
-									$row = $this->rowChatMessages(array( 'views' => $views));
-									$this->chat = array(	'views'	=> $views,
-															'row' 	=> $row,
-															'value' => $value
-																				);
-									$this->title = 'Архив';
-							}
-							// страница - Без темы
-							elseif($views=='wt-chats'){
-									$row = $this->rowChatMessages(array( 'views' => $views));
-									$this->chat = array(	'views'	=> $views,
-															'row' 	=> $row,
-															'value' => $value
-																				);
-									$this->title = 'Без темы';
-							}							
-							// удаляем оповещение(маркеры)
-							//reqDeleteNotification(array('notification_id'=>13));
-							
-							else{
-							
+                // Поиск
 
-								
-								//$this->e404 = '';
-								//$this->title = 'Ошибка 404. Страница не найдена';
-							}
-							
-							
-							
-				}else{
-					redirect('/');
-			}
-	}	
+                $row = $this->rowChatMessages(array('views' => $views,
+
+                    'value' => $value));
+
+                $rown = reqChatMessages(array('views' => $views,
+                    'folder_id' => $mid,
+                ));
+
+                $this->chat = array('views' => $views,
+                    'row' => $row,
+                    'value' => $value,
+                    'mid' => $mid,
+                    'rown' => $rown,
+                );
+                $this->title = 'Все сообщения';
+            }
+            // страница - Отктыре темы
+            elseif ($views == 'open-chats') {
+                $row = $this->rowChatMessages(array('views' => $views));
+                $this->chat = array('views' => $views,
+                    'row' => $row,
+                    'value' => $value,
+                );
+                $this->title = 'Отктыре темы';
+            }
+            // страница - Архивные
+            elseif ($views == 'arhive-chats') {
+                $row = $this->rowChatMessages(array('views' => $views));
+                $this->chat = array('views' => $views,
+                    'row' => $row,
+                    'value' => $value,
+                );
+                $this->title = 'Архив';
+            }
+            // страница - Без темы
+            elseif ($views == 'wt-chats') {
+                $row = $this->rowChatMessages(array('views' => $views));
+                $this->chat = array('views' => $views,
+                    'row' => $row,
+                    'value' => $value,
+                );
+                $this->title = 'Без темы';
+            }
+            // удаляем оповещение(маркеры)
+            //reqDeleteNotification(array('notification_id'=>13));
+
+            else {
+
+                //$this->e404 = '';
+                //$this->title = 'Ошибка 404. Страница не найдена';
+            }
+
+        } else {
+            redirect('/');
+        }
+    }
 }
-?>
