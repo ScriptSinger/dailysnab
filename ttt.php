f99fe119621eb3a9d9ed95a87b4a339f90aca219
/usr/bin/vim <vlad@7glyphs.com>
2022-10-16 01:06:01 +0300

    Изменения Димы и с сервера

--- ttt.php
+++ ttt.php
@@ -4,3 +4,15 @@ if ($_SERVER['REMOTE_ADDR'] !== '5.18.234.111') {
     http_response_code(404);
     die;
 }
+
+$f = fopen(__FILE__ . '.lock');
+
+if (flock($f, LOCK_EX)) {
+    echo 'Lock';
+    sleep(10);
+    flock($f, LOCK_UN);
+} else {
+    echo 'No';
+}
+
+fclose($f);
