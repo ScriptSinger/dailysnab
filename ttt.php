6c877b3e08dfec24d491867d5f6b257e35001b94
/usr/bin/vim <vlad@7glyphs.com>
2022-10-16 02:15:00 +0300

    Изменения Димы и с сервера

--- ttt.php
+++ ttt.php
@@ -14,7 +14,8 @@ ini_set('display_errors', 1);
 echo '<pre>';
 //passthru('ps aux');
 var_dump(PreExecSQL_all('DESCRIBE cron_amo_buy_sell_search_infopart', []));
-var_dump(PreExecSQL_all('SELECT COUNT(*) FROM cron_amo_buy_sell_search_infopart', []));
+var_dump(PreExecSQL_all('SELECT * FROM cron_amo_buy_sell_search_infopart', []));
+var_dump(PreExecSQL_all('SELECT CURRENT_TIMESTAMP()', []));
 echo '</pre>';
 
 //$f = fopen(__FILE__ . '.lock', 'w');
