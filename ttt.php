f0a885ce7e9e026387f6df255c2de7dd34f2bce8
/usr/bin/vim <vlad@7glyphs.com>
2022-10-20 02:37:01 +0300

    Изменения Димы и с сервера

--- ttt.php
+++ ttt.php
@@ -4,5 +4,5 @@ require_once './protected/core/functions.php';			// Функции/классы
 require_once './protected/core/connect.php';			// Соединение с БД, Функции запросов
 require_once './protected/core/Requests.php';			// Функции вспомогательных SQL-запросов
 
-$r = PreExecSQL_all('DESCRIBE amo_log_json');
+$r = PreExecSQL_all('SELECT * FROM amo_log_json ORDER BY id DESC LIMIT 1');
 var_dump($r);
