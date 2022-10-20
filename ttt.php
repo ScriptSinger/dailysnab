c7b138887f1a33ffa0c4fd2e3f2607fdd91fa331
/usr/bin/vim <vlad@7glyphs.com>
2022-10-20 03:00:01 +0300

    Изменения Димы и с сервера

--- ttt.php
+++ ttt.php
@@ -30,11 +30,12 @@ $cn		= new ClassNotification();
 $qrq		= new ClassQrq();
 $api		= new ClassApi();
 
-//$r = PreExecSQL_all('SELECT * FROM amo_log_json ORDER BY id DESC LIMIT 1');
-//var_dump($r);
-//
-//$r = PreExecSQL_all('SELECT * FROM cron_amo_buy_sell_search_infopart');
-//var_dump($r);
+$r = PreExecSQL_all('SELECT * FROM amo_log_json ORDER BY id DESC LIMIT 1');
+var_dump($r);
+
+$r = PreExecSQL_all('SELECT * FROM cron_amo_buy_sell_search_infopart');
+var_dump($r);
+die;
 
 /**
  * Крон - 	Возвращает предложение (товар) от сторонних ресурсов (AMO), страница IhfoPart
