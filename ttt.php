356f9dcb58cdd8163d76048877d90791847c1fe5
/usr/bin/vim <vlad@7glyphs.com>
2022-10-20 03:05:00 +0300

    Изменения Димы и с сервера

--- ttt.php
+++ ttt.php
@@ -30,7 +30,7 @@ $cn		= new ClassNotification();
 $qrq		= new ClassQrq();
 $api		= new ClassApi();
 
-$r = PreExecSQL_all('SELECT * FROM amo_log_json ORDER BY id DESC LIMIT 1');
+$r = PreExecSQL_all('SELECT * FROM amo_log_json ORDER BY id DESC LIMIT 5');
 var_dump($r);
 
 $r = PreExecSQL_all('SELECT * FROM cron_amo_buy_sell_search_infopart');
