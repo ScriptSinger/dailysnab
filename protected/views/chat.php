1966b4b45e6e86673ca66287a2319d1fd9ca9570
/usr/bin/vim <vlad@7glyphs.com>
2022-10-24 10:54:02 +0300

    Изменения Димы и с сервера

--- protected/views/chat.php
+++ protected/views/chat.php
@@ -11,7 +11,7 @@
 			
 		if ($_SERVER['REMOTE_ADDR'] == '178.214.251.82') {
 			echo "<pre>";
-			print_r($member['chat']['value']);
+			print_r($member['chat']['row']);
 			echo "<pre>";
 		}
 			// echo '<pre>';
