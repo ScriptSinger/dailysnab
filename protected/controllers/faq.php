a664980d413afd4ca067ca8d5af0648215863ec6
/usr/bin/vim <vlad@7glyphs.com>
2022-10-24 20:23:00 +0300

    Изменения Димы и с сервера

--- protected/controllers/faq.php
+++ protected/controllers/faq.php
@@ -16,6 +16,8 @@
 				$api->Save1cTransport(array('id_1c'=>'d46cc13b-995d-11e9-9415-000c29ba794a','company_id'=>1087));
 				
 				
+				
+				
 				/*
 				$url = 'https://questrequest.ru/qrq/amo/cartadd.php';
 				
