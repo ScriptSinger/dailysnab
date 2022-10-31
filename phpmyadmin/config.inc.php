f997298bcbeb711909823b9a358a0bce479573ec
/usr/bin/vim <vlad@7glyphs.com>
2022-10-31 17:04:02 +0300

    Изменения Димы и с сервера

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -27,13 +27,12 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'config';
 /* Server parameters */
-$cfg['Servers'][$i]['port'] = '3306';
-$cfg['Servers'][$i]['host'] = 'theylied.info';
+$cfg['Servers'][$i]['host'] = 'dailysnab.beget.tech';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
-$cfg['Servers'][$i]['user'] = 'udb3';
-$cfg['Servers'][$i]['password'] = 'rhger68gjfw2erg37wch7n';
+$cfg['Servers'][$i]['user'] = 'dailysnab_db';
+$cfg['Servers'][$i]['password'] = 'r*6bVbrm';
 
 /**
  * phpMyAdmin configuration storage settings.
