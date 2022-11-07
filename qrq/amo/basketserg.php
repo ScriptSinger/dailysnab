9f5a19c8c06ae63766b36bc86c5997ca12b70765
/usr/bin/vim <vlad@7glyphs.com>
2022-11-07 16:36:01 +0300

    Изменения Димы и с сервера

--- qrq/amo/basketserg.php
+++ qrq/amo/basketserg.php
@@ -18,7 +18,12 @@ fclose($fh);
   echo "<br><br>";
   echo "<pre>";
   //echo '111';
-    print_r( json_decode($text) );
+ //   print_r( json_decode($text) );
+ 
+  $Resp = json_decode($text,true) ;
+//  $pOsn = $Resp['Response']['entity']['baskets'][0]['basketForm']['fields'];
+  $pOsn = $Resp['Response'];
+  echo ($Resp);
   //echo '333';
   echo "</pre>";
   echo "<br><br>";
