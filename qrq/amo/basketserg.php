0acb12aac4726df72afb34fa99462b1d975f3d7c
/usr/bin/vim <vlad@7glyphs.com>
2022-11-07 16:26:02 +0300

    Изменения Димы и с сервера

--- qrq/amo/basketserg.php
+++ qrq/amo/basketserg.php
@@ -3,19 +3,20 @@
 $fd = fopen("basket.json", 'r') or die("не удалось открыть файл");
 while(!feof($fd))
 {
-//    $str = htmlentities(fgets($fd));
-  $str = fgets($fd);
-//    echo $str;
-      var_dump( json_encode($str) );
-
+    $str = htmlentities(fgets($fd));
+  //$str = fgets($fd);
+    echo $str;
+  
 
 }
 fclose($fd);
 
+  echo $str;
 
   echo "<br><br>";
   echo "<pre>";
   echo '111';
+    var_dump( $str[0] );
 
   echo '333';
   echo "</pre>";
