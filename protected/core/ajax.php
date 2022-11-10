507f1a148ded0a5677e918c0c466981452886c2d
/usr/bin/vim <vlad@7glyphs.com>
2022-11-10 14:39:02 +0300

    Изменения Димы и с сервера

--- protected/core/ajax.php
+++ protected/core/ajax.php
@@ -6182,7 +6182,7 @@ elseif($_GET['route'] == 'out_of_theme'){
 	if(($key = array_search(COMPANY_ID, $cs)) !== false || ($key = array_search(''.(-intval(COMPANY_ID)), $cs)) !== false){ //удаление элемента по значению
         $cs[$key] = ''.(-abs(intval($cs[$key])));
 	}
-    $cs = removeCompaniesFromListIfBanned(array_merge(array(COMPANY_ID), json_decode(json_encode($cs))), COMPANY_ID, array());
+    $cs = removeCompaniesFromListIfBanned(array_merge(array(COMPANY_ID), json_decode(json_encode($cs), true)), COMPANY_ID, array());
     $STH1 = PreExecSQL(" INSERT INTO tickets (folder_id,company_id,companies,ticket_exp,ticket_status) VALUES (?,?,?,?,?); " ,
         array($folder_id,COMPANY_ID,json_encode($cs),$messagetext,1)); // в сообщении компания отрицательная, значит уходит в архив
     $cs = removeCompaniesFromList($cs, COMPANY_ID, array(COMPANY_ID));
