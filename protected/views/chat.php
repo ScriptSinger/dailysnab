bb12828796d8cbd1102be77d72fa9100201fa0ce
/usr/bin/vim <vlad@7glyphs.com>
2022-11-12 15:52:02 +0300

    Изменения Димы и с сервера

--- protected/views/chat.php
+++ protected/views/chat.php
@@ -121,7 +121,7 @@ $last_message = [];
 						$out_of_theme = '<button type="button" class="button-blue pull-right block_of_theme" data-fid="'.$fid.'">Заблокировать пользователя</button>'; // уже после выхода из чата
 					} 				
 				elseif($theme != '')
-					{ //vecho(''.-abs($comp) . 'not in ' );
+					{ vecho(in_array_r( abs($comp), $rowf_comp ));
                       //vecho(end($rowf_comp));
 						$out_of_theme = '<button type="button" class="button-blue out_of_theme" data-fid="'.$fid.'">Выйти из темы</button>
 							<button type="button" class="button-blue close_theme_pr" data-fid="'.$fid.'">Предложить закрыть тему</button>'; //собеседников
